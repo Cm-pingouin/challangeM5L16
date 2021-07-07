0473af49f1f2f33ef6aa0b87455a712766879b89
Cm-pingouin <carlosmaloba4@gmail.com>
2021-07-07 15:44:27 +0200

    Initial Commit

---
+++ ChallengeM5L17/ChallengeM5L17App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeM5L17App.swift
+//  ChallengeM5L17
+//
+//  Created by Carlos Banza on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeM5L17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
